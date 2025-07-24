12c95a3eedd06c890b216a882048738d6b14c6f9
Ashutosh Apurva <apooorva01@gmail.com>
2025-07-24 04:39:07 +0000

    update: update ssh.py file

--- paramiko_module/ssh.py
+++ paramiko_module/ssh.py
@@ -1,8 +1,33 @@
 
 import paramiko
+import os
 
-# Create SSH client
-client = paramiko.SSHClient()
+def create_ssh_client():
+try:
 
-# Set policy to automatically add hosts to known hosts FileExistsError
-client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+  hostname = os.environ.get('SSH_HOSTNAME')
+  username = os.environ.get('SSH_USERNAME')
+  keyfile = os.environ.get('SSH_PRIVATE_KEY')
+  # Create SSH client
+  client = paramiko.SSHClient()
+
+  # Set policy to automatically add hosts to known hosts FileExistsError
+  client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+  # Connect to remote host
+  client.connect(hostname=hostname, username=username, key_filename=keyfile)
+  return client
+
+except paramiko.AuthenticationException:
+       print("Authentication Failed! Please check your hostname, username and Keyfile")
+
+
+except Exception as e:
+       print(f"Error Occured! {e}")
+    return none
+
+
+ssh_client = create_ssh_client()
+if ssh_client:
+    pass
+
