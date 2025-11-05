fa7540b11e9540f1f2774baeccb1b989d0ff7aeb
Ashutosh Apurva <apooorva01@gmail.com>
2025-11-05 02:52:01 +0000

    add string tasks

---
+++ daily_challenge/Day_1/GUIDE_ME.md
@@ -0,0 +1,4 @@
+# Day 1 of Python for DevOps Challenge
+
+## Data Type and its builtin functions
+
---
+++ daily_challenge/Day_1/strings_methods.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+# In this python script, I will play around strings and its builtin functions.
+
+
+#------------------------------------------------------------------------------------------#
+
+# Define a String and access it using indexing
+'''
+Indexing in python always start at 0 position (left side) and -1 at right position in any string.
+'''
+
+# Access the first character of a string
+
+name="hemant"
+
+print(name[0])
+
+
+# Access last character
+
+print(name[-1])
+
+
+# Print first 3 characters of the string
+
+print(name[0:3])
+
+
+# Write a Python program to calculate the length of a string.
+
+print(len(name))
