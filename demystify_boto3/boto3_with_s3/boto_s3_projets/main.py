f0a3c521d9fcfe0e47107cc9420c8a2de76a52b6
Ashutosh Apurva <apooorva01@gmail.com>
2026-01-25 02:52:02 +0000

    feat: boto module

---
+++ demystify_boto3/boto3_with_s3/boto_s3_projets/main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from boto-s3-projets!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ demystify_boto3/boto3_with_s3/connect_to_s3.py
@@ -0,0 +1,10 @@
+# Connect to AWS S3 bucket using boto3 module
+
+import boto3
+
+s3_client = boto3.client('s3')
+
+s3_resource = boto3.resource('s3')
+
+
+
