2cc8b88ee71e1a51d23a988180bf4e09136e13c3
AshuApurva14 <apooorva01@gmail.com>
2026-06-08 02:23:18 +0000

    refactor: enable input case-sensitive

--- network_output_formater.py
+++ network_output_formater.py
@@ -6,14 +6,14 @@ hostname = input("Hostname: ")
 port = input("Port: ")
 output_format = input("format: ")
 
-if output_format == "plain":
+if output_format == "plain" or "PLAIN" or "Plain":
     print(f"{ip_address}")
     print(f"{hostname}")
     print(f"{port}")
-elif output_format == "csv":
+elif output_format == "csv" or "CSV" or "Csv":
     print(f"The IP Address is: {ip_address}, Hostname: {hostname} and Port: {port}")
 
-elif output_format == "url":
+elif output_format == "url" or "URL" or "Url":
    print(f"https://{hostname}:{port}")
 
 else:
