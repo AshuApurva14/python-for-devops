22ddd21ab2b192ebdfb99e2271573ded4e6e9981
AshuApurva14 <apooorva01@gmail.com>
2026-06-08 02:47:29 +0000

    feat: add json format

--- network_output_formater.py
+++ network_output_formater.py
@@ -1,20 +1,32 @@
 #!/usr/bin/env python3
 
+""" Format and Print Coonection Info 
+"""
+
 
 ip_address = input("IP Address: ")
 hostname = input("Hostname: ")
-port = input("Port: ")
-output_format = input("format: ")
+port_text = input("Port: ")
+output_format = input("format: ").lower()
 
-if output_format == "plain" or "PLAIN" or "Plain":
+if not port_text.isdigit():
+    print("Invalid Port")
+else: 
+    port = (port_text)
+
+if output_format == "plain":
     print(f"{ip_address}")
     print(f"{hostname}")
     print(f"{port}")
-elif output_format == "csv" or "CSV" or "Csv":
+elif output_format == "csv":
     print(f"The IP Address is: {ip_address}, Hostname: {hostname} and Port: {port}")
 
-elif output_format == "url" or "URL" or "Url":
+elif output_format == "url":
    print(f"https://{hostname}:{port}")
+
+elif output_format == "json":
+    conn_info = '{"IP Address": "{ip_address}}'
+    print(f"{conn_info}")
 
 else:
     print("Unknown Format")
